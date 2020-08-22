b20a49112e0be256978421c39b1b1f48a22f4662
Darren Eberly <darren.eberly@gmail.com>
2020-08-22 17:45:09 -0400

    tests(window): removed tiling window manager test option

--- tests/unit2/test_window.py
+++ tests/unit2/test_window.py
@@ -15,9 +15,8 @@ def test_window(twm):
     assert w is not None
 
     # Make sure the arguments get passed to the window
-    if not twm:
-        assert w.width == width
-        assert w.height == height
+    assert w.width == width
+    assert w.height == height
     assert w.caption == title
     assert w.resizeable is resizable
     assert w.current_view is None
@@ -34,12 +33,11 @@ def test_window(twm):
 
     v = arcade.get_viewport()
 
-    if not twm:
-        assert v[0] == 0
-        # The lines below fail. Why?
-        assert v[1] == width - 1
-        assert v[2] == 0
-        assert v[3] == height - 1
+    assert v[0] == 0
+    # The lines below fail. Why?
+    assert v[1] == width - 1
+    assert v[2] == 0
+    assert v[3] == height - 1
 
     factor = arcade.get_scaling_factor()
     assert factor > 0
